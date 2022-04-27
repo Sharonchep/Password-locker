34cbc44fdd387541d76644fdd3f697a0eb2bd921
Sharonchep <sharon30@gmail.com>
2022-04-27 19:43:40 +0300

    add initial changes

--- run.py
+++ run.py
@@ -4,3 +4,26 @@ import random #Generate random numbers for various distributions including integ
 import string #string module allows you to create and customize your own string
 import getpass #The getpass module provides a secure way to handle the password prompts where programs interact with the users via the terminal.
 
+def create_user(name, user_password):
+    new_user = User(name, user_password)
+    return new_user
+def generate_password(user):
+    return user.generate_random_password()
+def save_user(user):
+    user.save_user()
+def delete_user(user):
+    user.delete_user()
+def create_credential(account, account_username, account_password):
+    new_credential = Credential(account, account_username, account_password)
+    return new_credential
+def save_credential(credential):
+    credential.save_credential()
+def delete_credential(credential):
+    credential.delete_credential()
+def find_credential(account_username):
+    return Credential.find_by_account_username(account_username)
+def check_existing_credential(account_username):
+    return Credential.find_by_account_username(account_username)
+def display_credential():
+    return Credential.display_all_credential()
+
