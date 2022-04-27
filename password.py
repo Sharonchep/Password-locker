cb771bcc428e86ed1b6494b36f03048587e7ccd8
Sharonchep <sharon30@gmail.com>
2022-04-27 18:54:10 +0300

    add credential list

--- password.py
+++ password.py
@@ -7,3 +7,14 @@ class User:
           User.user_list.append(self)
       def delete_user(self):
           User.user_list.remove(self)
+          
+class Credential:  
+      credential_list = []
+      def _init_(self,account,account_username,account_password):
+          self.account = account
+          self.account_username = account_username
+          self.account_password = account_password
+      def save_credential(self):
+          Credential.credential_list.append(self)
+      def delete_credential(self):
+          Credential.credential_list.remove(self)
