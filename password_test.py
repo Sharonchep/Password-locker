ac8dc23918980ed7d6fea1fe713ee96d0abb4bc1
Sharonchep <sharon30@gmail.com>
2022-04-27 17:58:07 +0300

    add new file

--- password_test.py
+++ password_test.py
@@ -6,4 +6,10 @@ class TestUser(unittest.TestCase):
          self.new_user = user("Sharon","2923")
      def tearDown(self):
          User.user_list = []
-         def test_init(self):
+         def test_init(self): 
+             self.assertEqual(self.new_user.name, "Sharon")
+             self.assertEqual(self.new_user.user_password, "2923")
+         def test_save_user(self):
+             self.new_user.save_user()
+             self.assertEqual(len(User.user_list),1)
+         def test_save_multiple_user(self):
