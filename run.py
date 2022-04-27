0e86af4f9c929d0e389ca83881c4250c8ae621da
Sharonchep <sharon30@gmail.com>
2022-04-27 19:41:04 +0300

    add password import user

---
+++ run.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3.8
+from password import User, Credential
+import random #Generate random numbers for various distributions including integer and floats.
+import string #string module allows you to create and customize your own string
+import getpass #The getpass module provides a secure way to handle the password prompts where programs interact with the users via the terminal.
+
